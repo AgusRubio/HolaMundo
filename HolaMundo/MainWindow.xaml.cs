57e77337ff15f49a785b18cd0da425ade44e3455
Agustin Rubio <agustin.rubio16@iesdoctorbalmis.com>
2021-09-23 12:02:54 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+
+namespace HolaMundo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string cadena;
+
+            cadena = CuadroDeTexto.Text;
+
+            Saludo.Text = "Hola " + cadena + "!";
+            Saludo.Foreground = Brushes.Green;
+        }
+    }
+}
